051a5a7ac5a0fd77902b4b472704636d2a3988a4
“Aariz <aariz120@gmail.com>
2024-02-29 10:11:03 +0530

    eda updated

--- demo.py
+++ demo.py
@@ -2,9 +2,13 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
 
+#logging.info("welcome to this project")
+
 try:
     a=1/"10"
     
 except Exception as e:
+    logging.info(e)
+    
     raise USvisaException(e,sys) from e
 
