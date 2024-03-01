c42dfba5edb584f2c1253b73ae13d338009ae135
“Aariz <aariz120@gmail.com>
2024-03-01 10:55:01 +0530

    ingestion updated

--- demo.py
+++ demo.py
@@ -1,14 +1,5 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
-#logging.info("welcome to this project")
 
-try:
-    a=1/"10"
-    
-except Exception as e:
-    logging.info(e)
-    
-    raise USvisaException(e,sys) from e
-
+pipline  = TrainPipeline()
+pipline.run_pipeline()
